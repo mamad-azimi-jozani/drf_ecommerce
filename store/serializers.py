850e1c5f13b909975a058eb56e3dff79f836d93d
mohammad-azimi <azimi.jozani.mohammad@gmail.com>
2023-02-25 11:26:45 +0330

    add custom method to my serializer

--- store/serializers.py
+++ store/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
 from .models import *
 
+from decimal import Decimal
 
 class ProductSerializer(serializers.ModelSerializer):
+    price_with_tax = serializers.SerializerMethodField(
+        method_name='get_price_with_tax'
+    )
     class Meta:
         model = Product
-        fields = ['id', 'title', 'slug', 'unit_price', 'inventory']
+        fields = ['id', 'title', 'slug', 'unit_price', 'price_with_tax', 'inventory']
+
+
+    def get_price_with_tax(self, obj: Product):
+        return obj.unit_price * Decimal(1.1)
