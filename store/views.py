38df48900cf08d372f42480f3f8671bce2c309d5
mohammad-azimi <azimi.jozani.mohammad@gmail.com>
2023-02-25 10:36:09 +0330

    pretending to write api for products

--- store/views.py
+++ store/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from .models import *
 
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+
+@api_view()
+def product_list(request):
+    data = {
+        "product": "niva",
+        "quantity": 5
+    }
+    return Response(data)
+
+@api_view()
+def product_detail(request, id):
+    data = {
+        "product": "niva",
+        "quantity": id
+    }
+    return Response(data)
