2866df9e4c132a039e53645d785b936056e1e08d
mohammad-azimi <azimi.jozani.mohammad@gmail.com>
2023-02-26 09:56:28 +0330

    write api for collection and collection detail

--- store/views.py
+++ store/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
 
-from .serializers import ProductSerializer
+from .serializers import *
 
 
 
@@ -41,3 +41,37 @@ def product_detail(request, id):
             return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(['GET', "POST"])
+def collection_list(request):
+    if request.method == 'GET':
+        collections = Collection.objects.annotate(products_count=Count('product')).all()
+
+        serializer = CollectionSerializer(collections, many=True)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        serializer = CollectionSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET', "PUT", "DELETE"])
+def collection_detail(request, id):
+    collection = get_object_or_404(Collection.objects.annotate(
+        products_count=Count('product')
+    ),  pk=id)
+    if request.method == 'GET':
+        serializer = CollectionSerializer(collection)
+        return Response(serializer.data)
+    if request.method == 'PUT':
+        serializer = CollectionSerializer(collection, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    if request.method == 'DELETE':
+        collection.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
