ed3686860c9f1235e08d50285c30c8b3a14173fb
mohammad-azimi <azimi.jozani.mohammad@gmail.com>
2023-02-26 08:13:14 +0330

    create test for list all products

--- store/tests.py
+++ store/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+from django.urls import reverse
+
+from .models import *
+
+from .serializers import ProductSerializer
+
+from rest_framework import status
+
+@pytest.mark.django_db
+def test_list_product(client):
+    url = reverse('product:product-lists')
+    response = client.get(url)
+
+    products = Product.objects.all()
+    expected_data = ProductSerializer(products, many=True)
+
+
+    assert response.status_code == status.HTTP_200_OK
+    assert expected_data.data == response.data
+
+
+# @pytest.mark.django_db
+# def test_detail_product(client, product: Product):
+#     url = reverse('product:product-detail', kwargs={'pk': product.pk})
+#     response = client.get(url)
+#
+#     product = Product.objects.get(pk=product.pk)
+#     serializer = ProductSerializer(product)
+#
+#     assert response.status_code == 200
+#     assert response.data == serializer.data
+
--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import *
 
+
+app_name = 'product'
+
 urlpatterns = [
-    path('products/', product_list),
-    path('products/<int:id>/', product_detail)
+    path('products/', product_list, name='product-lists'),
+    path('products/<int:id>/', product_detail, name='product-detail')
 ]
