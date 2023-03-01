18ab5c4e264d51f10257436d9a8a60d7613477b6
mohammad-azimi <azimi.jozani.mohammad@gmail.com>
2023-03-01 08:46:06 +0330

    add pagination to my api

--- store/views.py
+++ store/views.py
@@ -1,22 +1,18 @@
-from .models import *
-
-
 from rest_framework.viewsets import ModelViewSet
-
-
-from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter
 
+from django_filters.rest_framework import DjangoFilterBackend
+
 from .serializers import *
-
 from .filters import ProductFilter
-
-
+from .models import *
+from .paginations import DefaultPagination
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend, OrderingFilter]
+    pagination_class = DefaultPagination
     filterset_fields = ['collection_id']
     filterset_class = ProductFilter
     ordering_fields = ['unit_price']
